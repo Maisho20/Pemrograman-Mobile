9a36e64400fb961579ad3d2f7136d4fd36ef69d6
Sabbaha Naufal <95173258+Maisho20@users.noreply.github.com>
2023-10-19 10:49:04 +0700

    Delete Week 8/membangun_navigasi_di_flutter directory

--- Week 8/membangun_navigasi_di_flutter/README.md
+++
@@ -1,3 +0,0 @@
-# membangun_navigasi_di_flutter
-
-**Langkah 1: Siapkan project baru**
--- Week 8/membangun_navigasi_di_flutter/android/app/src/main/kotlin/com/example/membangun_navigasi_di_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.membangun_navigasi_di_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
