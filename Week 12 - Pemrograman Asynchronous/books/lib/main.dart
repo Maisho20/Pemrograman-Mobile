c374ae3feaa228446535e6abc314f3897b49857d
Maisho20 <bahajazz70@gmail.com>
2023-11-27 15:27:57 +0700

    Pemrograman Mobile Minggu 12 - Soal 1

---
+++ Week 12 - Pemrograman Asynchronous/books/lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Future demo Baha',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: const FuturePage(),
+    );
+  }
+}
+
+// https://github.com/Maisho20/Pemrograman-Mobile.git
+class FuturePage extends StatefulWidget {
+  const FuturePage({Key? key}) : super(key: key);
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  Future<Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const Path = '/books/v1/volumes';
+    Uri url = Uri.https(authority, Path);
+    return http.get(url);
+  }
+
+  String result = '';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Back from Future Demo'),
+      ),
+      body: Center(
+        child: Column(children: [
+          const Spacer(),
+          ElevatedButton(
+            child: const Text('GO!'),
+            onPressed: () {},
+          ),
+          const Spacer(),
+          Text(result),
+          const Spacer(),
+          const CircularProgressIndicator(),
+          const Spacer(),
+        ]),
+      ),
+    );
+  }
+}
